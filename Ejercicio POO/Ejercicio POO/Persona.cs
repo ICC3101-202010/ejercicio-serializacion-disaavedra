d299bbb76f9460421fa9454650633fc76b7795fe
Daniel <disaavedra5@gmail.com>
2020-05-07 09:26:31 -0400

    Primer Commit

---
+++ Ejercicio POO/Ejercicio POO/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_POO
+{
+    class Persona
+    {
+        private String nombre;
+
+        private String apellido;
+
+        private String edad;
+
+        Persona(String nombre, String apellido, String edad)
+        {
+            this.nombre = nombre;
+
+            this.apellido = apellido;
+
+            this.edad = edad;
+        }
+    }
+}
